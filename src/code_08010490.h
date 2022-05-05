865f514d28c1d455be56110a96b63cfb00ecdf14
someone2639 <someone2639@gmail.com>
2022-05-05 14:58:01 -0400

    more repo consistency!

--- src/code_08010490.h
+++ src/code_08010490.h
@@ -1,12 +1,12 @@
 #pragma once
 
-typedef struct {
+struct struct_08011a18 {
 	u8 unk0;
 	u8 unk1[10];
 	u8 unkB;
 	u16 unkC[10];
 	u32 unk20;
-} struct_08011a18;
+};
 
 // extern ? func_08010490(?);
 // extern ? func_08010494(?);
@@ -62,13 +62,13 @@ typedef struct {
 // extern ? func_08011744(?);
 // extern ? func_08011820(?);
 // extern ? func_08011a08(?);
-extern void func_08011a18(struct_08011a18 *arg0);
+extern void func_08011a18(struct struct_08011a18 *arg0);
 // extern ? func_08011a58(?);
 // extern ? func_08011a90(?);
 // extern ? func_08011ad4(?);
 // extern ? func_08011b50(?);
-extern int func_08011bec(u8 *param_1);
-extern int func_08011bf8(struct_08011a18 *arg0);
+extern s32 func_08011bec(u8 *param_1);
+extern s32 func_08011bf8(struct struct_08011a18 *arg0);
 // extern ? func_08011c1c(?);
 // extern ? func_08011d8c(?);
 // extern ? func_08011e74(?);
