f67e1ab460ab7bb64be785f2a00cc6ea778af977
someone2639 <someone2639@gmail.com>
2022-05-05 14:46:21 -0400

    struct fields too

--- src/code_08010490.c
+++ src/code_08010490.c
@@ -126,14 +126,14 @@ asm(".include \"include/gba.inc\"");//Temporary
 void func_08011a18(struct_08011a18 *arg0) {
     u32 i;
 
-    arg0->_000 = 0;
+    arg0->unk0 = 0;
     
     for (i = 0; i < 10; i++) {
-        arg0->_001[i] = 0;
-        arg0->_00C[i] = 0;
+        arg0->unk1[i] = 0;
+        arg0->unkC[i] = 0;
     }
 
-    func_080018e0(0, &arg0->_020, 0x3800, 0x20, 0x200);
+    func_080018e0(0, &arg0->unk20, 0x3800, 0x20, 0x200);
 }
 
 #include "asm/code_08010490/asm_08011a58.s"
@@ -152,7 +152,7 @@ int func_08011bf8(struct_08011a18 *arg0) {
     u32 i;
 
     for (i = 0; i < 10; i++) {
-        if (arg0->_00C[i] == 0) {
+        if (arg0->unkC[i] == 0) {
             return i;
         }
     }
