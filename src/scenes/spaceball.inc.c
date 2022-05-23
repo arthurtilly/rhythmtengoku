ac594a384019166868ad060597e1e32732fb5358
GenericArrangements <genericarrangements.wah@gmail.com>
2022-05-24 00:29:45 +1000

    Decompile... technically nothing.

--- src/scenes/spaceball.inc.c
+++ src/scenes/spaceball.inc.c
@@ -1,43 +1,67 @@
+#include "src/code_08001360.h"
+#include "src/code_08007468.h"
+#include "src/code_0800b778.h"
+
+
+
 #include "asm/scenes/spaceball/asm_0801fc44.s"
 
 #include "asm/scenes/spaceball/asm_0801fcb0.s"
 
 #include "asm/scenes/spaceball/asm_0801fd1c.s"
 
+// [func_0801fd70] Update Entity Position/Scaling (https://decomp.me/scratch/wjRxD)
 #include "asm/scenes/spaceball/asm_0801fd70.s"
 
+// [func_0801fdc4] Update Batter Position/Scaling (https://decomp.me/scratch/mXBsv)
 #include "asm/scenes/spaceball/asm_0801fdc4.s"
 
+// [func_0801fe6c] Update Various (https://decomp.me/scratch/MxokI)
 #include "asm/scenes/spaceball/asm_0801fe6c.s"
 
+// [func_0801ff60] GFX Init Func_00 (https://decomp.me/scratch/zDRwb)
 #include "asm/scenes/spaceball/asm_0801ff60.s"
 
+// [func_0801ff70] GFX Init Func_01 (https://decomp.me/scratch/mW2EL)
 #include "asm/scenes/spaceball/asm_0801ff70.s"
 
+// [func_0801ffa0] GFX Init Func_00 (https://decomp.me/scratch/fCFe6)
 #include "asm/scenes/spaceball/asm_0801ffa0.s"
 
+// [func_0801ffcc] MAIN - Init (https://decomp.me/scratch/nK94B)
 #include "asm/scenes/spaceball/asm_0801ffcc.s"
 
+// [func_080201a0] SCENE Func_06 - STUB
 #include "asm/scenes/spaceball/asm_080201a0.s"
 
+// [func_080201a4] SCENE Func_00 - ?? (Pitcher Animation) (https://decomp.me/scratch/4IhaO)
 #include "asm/scenes/spaceball/asm_080201a4.s"
 
+// [func_080201cc] Update Batter (https://decomp.me/scratch/erUhA)
 #include "asm/scenes/spaceball/asm_080201cc.s"
 
+// [func_08020200] SCENE Func_01 - Loop Exit Condition (Practice) (https://decomp.me/scratch/sYz0I)
 #include "asm/scenes/spaceball/asm_08020200.s"
 
+// [func_08020238] SCENE Func_02 - Set Camera Zoom (https://decomp.me/scratch/k0PS5)
 #include "asm/scenes/spaceball/asm_08020238.s"
 
+// [func_0802026c] SCENE Func_03 - Set Batter Variant (https://decomp.me/scratch/Lg362)
 #include "asm/scenes/spaceball/asm_0802026c.s"
 
+// [func_08020290] SCENE Func_05 - Set Next Spaceball Sprite (https://decomp.me/scratch/qNuLu)
 #include "asm/scenes/spaceball/asm_08020290.s"
 
+// [func_080202a4] SCENE Func_04 - Set Umpire Animation (https://decomp.me/scratch/y7Y2K)
 #include "asm/scenes/spaceball/asm_080202a4.s"
 
+// [func_080202f0] MAIN - Update (https://decomp.me/scratch/zgr0F)
 #include "asm/scenes/spaceball/asm_080202f0.s"
 
+// [func_08020308] STUB
 #include "asm/scenes/spaceball/asm_08020308.s"
 
+// [func_0802030c] CUE - Spawn
 #include "asm/scenes/spaceball/asm_0802030c.s"
 
 #include "asm/scenes/spaceball/asm_080203fc.s"
@@ -46,18 +70,26 @@
 
 #include "asm/scenes/spaceball/asm_08020500.s"
 
+// [func_08020564] CUE - Behaviour
 #include "asm/scenes/spaceball/asm_08020564.s"
 
+// [func_080205a0] CUE - Despawn
 #include "asm/scenes/spaceball/asm_080205a0.s"
 
+// [func_080205ac] CUE - Hit
 #include "asm/scenes/spaceball/asm_080205ac.s"
 
+// [func_080205e8] CUE - Barely
 #include "asm/scenes/spaceball/asm_080205e8.s"
 
+// [func_08020644] CUE - Miss
 #include "asm/scenes/spaceball/asm_08020644.s"
 
+// [func_08020660] MAIN - Input Event
 #include "asm/scenes/spaceball/asm_08020660.s"
 
+// [func_08020698] COMMON Func_00 - STUB
 #include "asm/scenes/spaceball/asm_08020698.s"
 
+// [func_0802069c] COMMON Func_00 - STUB
 #include "asm/scenes/spaceball/asm_0802069c.s"
