10ee03c31f37962e4be2a768a34f44c388aa0b60
GenericArrangements <genericarrangements.wah@gmail.com>
2022-08-19 09:37:31 +1000

    Decompile animations and palettes for Rhythm Library, Cafe and Endless Game Menu Icons. Decompile BG palettes for epilogues.

---
+++ graphics/extra_game_menus/endless_game_icons_anim.c
@@ -0,0 +1,28 @@
+#include "global.h"
+#include "graphics.h"
+
+#include "endless_game_icons_anim_cells.inc.c"
+
+// [D_08900444] <description>
+const struct Animation endless_game_icons_anim00[] = {
+    /* 000 */ { endless_game_icons_cel002, 4 },
+    /* End */ END_ANIMATION
+};
+
+// [D_08900454] <description>
+const struct Animation endless_game_icons_anim01[] = {
+    /* 000 */ { endless_game_icons_cel000, 4 },
+    /* End */ END_ANIMATION
+};
+
+// [D_08900464] <description>
+const struct Animation endless_game_icons_anim02[] = {
+    /* 000 */ { endless_game_icons_cel003, 4 },
+    /* End */ END_ANIMATION
+};
+
+// [D_08900474] <description>
+const struct Animation endless_game_icons_anim03[] = {
+    /* 000 */ { endless_game_icons_cel001, 4 },
+    /* End */ END_ANIMATION
+};
