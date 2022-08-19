3e9d0e12a134902842aae81106ed8f12fbb32a6a
GenericArrangements <genericarrangements.wah@gmail.com>
2022-08-19 10:57:12 +1000

    Decompile animations and palettes for Studio (SRAM Usage Indicator), Drum Lessons Menu Icons and Rhythm Data Room Menu.

--- graphics/extra_game_menus/endless_game_icons_anim.c
+++ graphics/extra_game_menus/endless_game_icons_anim.c
@@ -3,25 +3,25 @@
 
 #include "endless_game_icons_anim_cells.inc.c"
 
-// [D_08900444] <description>
+// [D_08900444] Mr. Upbeat
 const struct Animation endless_game_icons_anim00[] = {
     /* 000 */ { endless_game_icons_cel002, 4 },
     /* End */ END_ANIMATION
 };
 
-// [D_08900454] <description>
+// [D_08900454] Mannequin Factory
 const struct Animation endless_game_icons_anim01[] = {
     /* 000 */ { endless_game_icons_cel000, 4 },
     /* End */ END_ANIMATION
 };
 
-// [D_08900464] <description>
+// [D_08900464] Quiz Show EX
 const struct Animation endless_game_icons_anim02[] = {
     /* 000 */ { endless_game_icons_cel003, 4 },
     /* End */ END_ANIMATION
 };
 
-// [D_08900474] <description>
+// [D_08900474] Sick Beats SP
 const struct Animation endless_game_icons_anim03[] = {
     /* 000 */ { endless_game_icons_cel001, 4 },
     /* End */ END_ANIMATION
