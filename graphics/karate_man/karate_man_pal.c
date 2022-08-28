fcef962320abf3199c2e356d7bd06450613113b1
GenericArrangements <genericarrangements.wah@gmail.com>
2022-08-28 21:45:15 +1000

    Decompile animations and palettes for Tram & Pauline (Tutorial), Tram & Pauline, Horse Machine and Sick Beats. Rename Karate Man palettes.

--- graphics/karate_man/karate_man_pal.c
+++ graphics/karate_man/karate_man_pal.c
@@ -2,7 +2,7 @@
 #include "graphics.h"
 
 // [D_088ad004] <description>
-const Palette karate_man_v0_pal[] = {
+const Palette karate_man_pal[] = {
     /* PALETTE 00 */ {
         /* 00 */ TO_RGB555(0x18F8E0),
         /* 01 */ TO_RGB555(0x000000),
@@ -150,7 +150,7 @@ const Palette karate_man_v0_pal[] = {
 };
 
 // [D_088ad104] <description>
-const Palette karate_man_v3_pal[] = {
+const Palette karate_man_2_pal[] = {
     /* PALETTE 00 */ {
         /* 00 */ TO_RGB555(0x18F8E0),
         /* 01 */ TO_RGB555(0x000000),
