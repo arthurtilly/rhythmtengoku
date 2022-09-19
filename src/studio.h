3ace62e296964d1edee96c040baa229c0493e405
GenericArrangements <genericarrangements.wah@gmail.com>
2022-09-19 11:49:19 +1000

    Split C files, header files and asm files for Reading, Studio, Title Screen and Medal Corner Menus.

---
+++ src/studio.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "global.h"
+#include "scenes.h"
+
+// extern ? func_0801a9c8(?); // [func_0801a9c8] STUDIO MENU - ?
+// extern ? func_0801aa04(?);
+// extern ? func_0801aaa0(?);
+// extern ? func_0801ac38(?);
+// extern ? func_0801ad44(?);
+// extern ? func_0801ad9c(?);
+// extern ? func_0801adf0(?);
+// extern ? func_0801ae34(?);
+// extern ? func_0801aea4(?);
+// extern ? func_0801aed0(?);
+// extern ? func_0801af64(?);
+// extern ? func_0801aff8(?);
+// extern ? func_0801b0b0(?);
+// extern ? func_0801b138(?);
+// extern ? func_0801b1d8(?);
+// extern ? func_0801b498(?);
+// extern ? func_0801b4b4(?);
+// extern ? func_0801b4f4(?);
+// extern ? func_0801b668(?);
+// extern ? func_0801b6fc(?);
+// extern ? func_0801b710(?);
+// extern ? func_0801b7b8(?);
+// extern ? func_0801b834(?);
+// extern ? func_0801b858(?);
+// extern ? func_0801b9ec(?);
+// extern ? func_0801ba60(?);
+// extern ? func_0801ba74(?);
+// extern ? func_0801bb24(?);
+// extern ? func_0801bb60(?);
+// extern ? func_0801bbd4(?);
+// extern ? func_0801bbe8(?);
+// extern ? func_0801bc0c(?);
+// extern ? func_0801bce0(?);
+// extern ? func_0801c028(?);
+// extern ? func_0801c1b8(?); // [func_0801c1b8] STUDIO MENU - Initialise Static Variables
+// extern ? func_0801c1ec(?); // [func_0801c1ec] STUDIO MENU - Graphics Init. 3
+// extern ? func_0801c234(?); // [func_0801c234] STUDIO MENU - Graphics Init. 2
+// extern ? func_0801c260(?); // [func_0801c260] STUDIO MENU - Graphics Init. 1
+// extern ? func_0801c290(?); // [func_0801c290] STUDIO MENU - Graphics Init. 0
+// extern ? func_0801c2ec(?); // [func_0801c2ec] STUDIO MENU - Scene Init.
+// extern ? func_0801c504(?);
+// extern ? func_0801c510(?);
+// extern ? func_0801c51c(?);
+// extern ? func_0801c530(?);
+// extern ? func_0801c5a0(?); // [func_0801c5a0] STUDIO MENU - Scene STUB
+// extern ? func_0801c5a4(?);
+// extern ? func_0801c674(?);
+// extern ? func_0801c6b8(?);
+// extern ? func_0801c6fc(?);
+// extern ? func_0801c7c0(?);
+// extern ? func_0801c7e8(?);
+// extern ? func_0801c7ec(?); // [func_0801c7ec] STUDIO MENU - Scene Main
+// extern ? func_0801c8b8(?);
+// extern ? func_0801c918(?); // [func_0801c918] STUDIO MENU - Scene Close
+// extern ? func_0801c930(?); // [func_0801c930] STUDIO MENU - ? (Script Function)
+// extern ? func_0801c944(?); // [func_0801c944] STUDIO MENU - ? (Script Function)
+// extern ? func_0801c960(?);
+// extern ? func_0801c96c(?);
+// extern ? func_0801c99c(?);
+// extern ? func_0801c9b8(?);
+// extern ? func_0801c9c4(?);
+// extern ? func_0801c9ec(?);
+// extern ? func_0801ca08(?);
+// extern ? func_0801ca20(?);
