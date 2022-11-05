28c74f83d9b607d0e3243a88d1c985496b0e0a22
GenericArrangements <genericarrangements.wah@gmail.com>
2022-11-05 21:24:00 +1100

    Decompile all remaining Cafe dialogue.

--- data/data_0804f338.c
+++ data/data_0804f338.c
@@ -649,3 +649,151 @@ const char D_080506d8[] =
     "すこしは　休んでくださいね。\n"
     "\n";
 
+// <empty>
+const char D_08050714[] = "";
+
+// <new line>
+const char D_08050718[] = "\n";
+
+// Was that you on [...
+const char D_0805071c[] = "そうそう、";
+
+// Shadowed Text
+const char D_08050728[] = "\x05\x31" "\x01\x35";
+
+// ...]?
+const char D_08050730[] = "\x05\x34" "\x01\x38" "で\n";
+
+// I heard you just got a Perfect!?
+const char D_08050738[] = "パーフェクト　だしたんだって！？";
+
+// You're still on [...]
+const char D_0805075c[] = "そういえば、";
+
+// Are you having trouble?
+const char D_0805076c[] = "行きづまってませんか？\n" "\n";
+
+// Hmm... is [...]
+const char D_08050788[] = "う～む…　";
+
+// [...] giving...
+const char D_08050794[] = "\x05\x34" "\x01\x38" "に\n";
+
+// ...you trouble earning that medal?
+const char D_0805079c[] = "てこずってるんですねぇ。\n" "\n";
+
+// Rumor has it [...]
+const char D_080507b8[] = "ウワサを　きいたんですけど、\n";
+
+// [...] was doing a perfect campaign.
+const char D_080507d8[] = "\x05\x34" "\x01\x38" "　ばかり\n";
+
+// Weren't you playing it just now?
+const char D_080507e8[] = "してるみたいですね。";
+
+// Ah! Wait, I remember!
+const char D_08050800[] =
+    "\n"
+    "\n"
+    "あ、　ちょっと　思い出しました！\n"
+    "\n";
+
+// Hey, here's a tip!
+// Soon [...]
+const char D_08050828[] =
+    "そうそう、\n"
+    "もうすぐ";
+
+// [...] is going
+// to be having a perfect campaign.
+const char D_0805083c[] =
+    "\x05\x34" "\x01\x38" "で、\n"
+    "パーフェクトキャンペーンを\n"
+    "するそうですヨ。";
+
+// Please keep on working hard.
+// I'll be here rooting for you~!
+const char D_08050874[] =
+    "\n"
+    "これからも　がんばってね。\n"
+    "おうえん　してるからね～！\n"
+    "\n";
+
+// If you like, I could let you
+// <skip that game> if you want to...
+//
+// "Please skip it."
+// "No, thank you."
+const char D_080508b0[] =
+    "う～ん、もし　よければ　なんだけどね、\n"
+    "\x05\x31" "\x01\x35" "進めるようにしておく" "\x05\x34" "\x01\x38" "けど…？\n"
+    "\n"
+    "　　　　　　　　「おねがいします」\n"
+    "　　　　　　　　「けっこうです」";
+
+// <Leave it to me!>
+const char D_08050944[] =
+    "\n"
+    "\n"
+    "\x03\x32" "\x01\x6c" "\x05\x31" "\x01\x35" "まかせとき！" "\x03\x30" "\x01\x73" "\x05\x34" "\x01\x38";
+
+// Oh, is that so?
+// You're determined to beat
+// it with your own strength.
+// Excellent!
+const char D_08050964[] =
+    "そうですか。\n"
+    "じぶんの　チカラで\n"
+    "がんばるんですね。\n"
+    "エライ！！";
+
+// I hope the next game will go
+// much better for you.
+// Tell me about it next time.
+const char D_080509a4[] =
+    "\n"
+    "次のゲーム　どんなのでしょうね。\n"
+    "うまくできると　いいね～。\n"
+    "ではまた。";
+
+// Well then, please
+// do your best!
+const char D_080509ec[] =
+    "\n"
+    "\n"
+    "では　がんばってくださいね～。\n"
+    "\n";
+
+// Were you practicing for the
+// <Perfect Campaign>?
+//
+//         "That's right."
+//         "Not right now."
+const char D_08050a10[] =
+    "\x05\x31" "\x01\x35" "パーフェクト　キャンペーン" "\x05\x34" "\x01\x38" "　に　そなえて　れんしゅうですか？\n"
+    "\n"
+    "　　　　　　　　「そうなんです」\n"
+    "　　　　　　　　「ちがいますヨ」";
+
+// Just try your best and
+// go get that Perfect!
+const char D_08050a98[] =
+    "\n"
+    "がんばって　パーフェクト、\n"
+    "とってくださいネ！\n"
+    "\n";
+
+// At long last...
+// You have completed
+// the Perfect Campaign!
+const char D_08050acc[] =
+    "\n"
+    "とうとう、\n"
+    "パーフェクト　キャンペーンを\n"
+    "コンプリートしたんだって！？";
+
+// Crazy awesome!!
+const char D_08050b14[] =
+    "\x03\x32" "\x01\x6c" "\x05\x31" "\x01\x35" "\n"
+    "\n"
+    "めちゃすごーい!!" "\x03\x30" "\x01\x73" "\x05\x34" "\x01\x38";
