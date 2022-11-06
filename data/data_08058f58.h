8d28d8f608009c456c93571e4b451a8e7728d135
GenericArrangements <genericarrangements.wah@gmail.com>
2022-11-06 20:58:30 +1100

    Fully decompile data_08058f58.s.

---
+++ data/data_08058f58.h
@@ -0,0 +1,120 @@
+#pragma once
+
+// Studio - ?
+extern const char D_08058f58[];
+
+// Studio - Song Titles
+extern const char D_08058f60[];
+extern const char D_08058f70[];
+extern const char D_08058f7c[];
+extern const char D_08058f84[];
+extern const char D_08058f90[];
+extern const char D_08058fa0[];
+extern const char D_08058fb4[];
+extern const char D_08058fc8[];
+extern const char D_08058fd8[];
+extern const char D_08058fe8[];
+extern const char D_08058ff8[];
+extern const char D_08059004[];
+extern const char D_08059010[];
+extern const char D_08059020[];
+extern const char D_08059030[];
+extern const char D_08059040[];
+extern const char D_08059048[];
+extern const char D_08059058[];
+extern const char D_0805906c[];
+extern const char D_08059078[];
+extern const char D_08059088[];
+extern const char D_08059090[];
+extern const char D_080590a0[];
+extern const char D_080590ac[];
+extern const char D_080590bc[];
+extern const char D_080590cc[];
+extern const char D_080590dc[];
+extern const char D_080590e8[];
+extern const char D_080590f8[];
+extern const char D_08059108[];
+extern const char D_08059114[];
+extern const char D_08059124[];
+extern const char D_08059134[];
+extern const char D_08059144[];
+extern const char D_08059150[];
+extern const char D_08059160[];
+extern const char D_0805916c[];
+extern const char D_0805917c[];
+extern const char D_0805918c[];
+extern const char D_0805919c[];
+extern const char D_080591b0[];
+extern const char D_080591c4[];
+extern const char D_080591e4[];
+extern const char D_080591f4[];
+extern const char D_08059204[];
+extern const char D_08059214[];
+extern const char D_08059220[];
+
+// Studio - Drum Kit Titles
+extern const char D_0805922c[];
+extern const char D_0805923c[];
+extern const char D_0805924c[];
+extern const char D_0805925c[];
+extern const char D_0805926c[];
+extern const char D_08059284[];
+extern const char D_0805929c[];
+extern const char D_080592b0[];
+extern const char D_080592c0[];
+extern const char D_080592d0[];
+extern const char D_080592e0[];
+extern const char D_080592f0[];
+extern const char D_08059300[];
+extern const char D_08059310[];
+extern const char D_08059320[];
+
+// Studio - Misc. Text
+extern const char D_08059330[];
+extern const char D_08059334[];
+extern const char D_08059338[];
+extern const char D_08059340[];
+extern const char D_08059374[];
+extern const char D_080593b4[];
+extern const char D_080593ec[];
+extern const char D_080593f8[];
+extern const char D_08059404[];
+extern const char D_08059410[];
+extern const char D_0805941c[];
+extern const char D_08059428[];
+extern const char D_0805942c[];
+extern const char D_0805945c[];
+extern const char D_080594a0[];
+
+// Medal Corner - Rhythm Toy Titles
+extern const char D_080594cc[];
+extern const char D_080594dc[];
+extern const char D_080594ec[];
+extern const char D_080594fc[];
+
+// Medal Corner - Endless Game Titles
+extern const char D_0805950c[];
+extern const char D_0805951c[];
+extern const char D_08059530[];
+extern const char D_08059544[];
+
+// Medal Corner - Drum Lesson Titles
+extern const char D_08059550[];
+extern const char D_08059560[];
+extern const char D_08059570[];
+extern const char D_0805957c[];
+extern const char D_08059588[];
+extern const char D_08059594[];
+extern const char D_080595a0[];
+extern const char D_080595ac[];
+extern const char D_080595b8[];
+extern const char D_080595c8[];
+extern const char D_080595d8[];
+extern const char D_080595e8[];
+extern const char D_080595f8[];
+extern const char D_08059608[];
+extern const char D_08059618[];
+extern const char D_08059628[];
+extern const char D_08059638[];
+extern const char D_08059648[];
+extern const char D_08059658[];
---
+++ data/data_08059668.c
@@ -0,0 +1,249 @@
+#include "global.h"
+#include "data_08059668.h"
+
+
+/* Debug Menu */
+
+
+const char D_08059668[] = "＿";
+
+// (Lots of stuff from here on isn't translated, but it's mostly game names anyway)
+const char D_0805966c[] = "シーケンス　テスト";
+
+const char D_08059680[] = "＊";
+
+const char D_08059684[] = "１／１";
+
+// Data Clear
+const char D_0805968c[] = "データクリア";
+
+const char D_0805969c[] = "エンディング（おまけから）";
+
+const char D_080596b8[] = "エンディング（クリアご）";
+
+const char D_080596d4[] = "Ｒ－ＩＱ　（おもちゃ）";
+
+const char D_080596ec[] = "Ｒ－ＩＱ　（ドラムレッスン）";
+
+const char D_0805970c[] = "Ｒ－ＩＱ　（ミニゲーム）";
+
+const char D_08059728[] = "Ｒ－ＩＱ　（カウンセラー）";
+
+const char D_08059744[] = "Ｒ－ＩＱ　（パーフェクト）";
+
+const char D_08059760[] = "Ｒ－ＩＱ　（にゅうか）";
+
+const char D_08059778[] = "Ｒ－ＩＱ　（リザルト４）";
+
+const char D_08059794[] = "Ｒ－ＩＱ　（リザルト３）";
+
+const char D_080597b0[] = "Ｒ－ＩＱ　（リザルト２）";
+
+const char D_080597cc[] = "Ｒ－ＩＱ　（リザルト）";
+
+const char D_080597e4[] = "Ｒ－ＩＱ　（オプション）";
+
+const char D_08059800[] = "Ｒ－ＩＱ　（スタジオ）";
+
+const char D_08059818[] = "Ｒ－ＩＱ　（しりょうしつ）";
+
+const char D_08059834[] = "Ｒ－ＩＱ　（ゲーム）";
+
+const char D_0805984c[] = "Ｒ－ＩＱ　（メニュー）";
+
+const char D_08059864[] = "Ｒ－ＩＱ　（タイトル）";
+
+// Karate Man 2
+const char D_0805987c[] = "カラテカ　２";
+
+// Karate Man
+const char D_0805988c[] = "カラテカ";
+
+// The Snappy Trio
+const char D_08059898[] = "バリバリ３にんしゅう";
+
+// The Clappy Trio
+const char D_080598b0[] = "パチパチ３にんしゅう";
+
+const char D_080598c8[] = "ポリリズム　２";
+
+const char D_080598d8[] = "ポリリズム";
+
+const char D_080598e4[] = "ナイト　ウォーク　２";
+
+const char D_080598fc[] = "ナイト　ウォーク";
+
+const char D_08059910[] = "リズムだつもう　２";
+
+const char D_08059924[] = "リズムだつもう";
+
+const char D_08059934[] = "バイキン　はかせ";
+
+const char D_08059948[] = "ホッピングロード　２";
+
+const char D_08059960[] = "ホッピングロード";
+
+const char D_08059974[] = "ニンジャ　２";
+
+const char D_08059984[] = "ニンジャ";
+
+const char D_08059990[] = "しろいおばけ　２";
+
+const char D_080599a4[] = "しろいおばけ";
+
+const char D_080599b4[] = "エアーバッター２";
+
+const char D_080599c8[] = "エアーバッター";
+
+const char D_080599d8[] = "いあいぎり";
+
+const char D_080599e4[] = "スーパータップ";
+
+const char D_080599f4[] = "タップダンズ";
+
+const char D_08059a04[] = "うま（スピード）";
+
+const char D_08059a18[] = "マーチャ　２";
+
+const char D_08059a28[] = "マーチャ";
+
+const char D_08059a34[] = "まほうつかい";
+
+const char D_08059a44[] = "ダンスレッスン　１";
+
+const char D_08059a58[] = "ウサギとび";
+
+const char D_08059a64[] = "メトロノーム";
+
+const char D_08059a74[] = "ハナビ";
+
+const char D_08059a7c[] = "フラッシュメモリテスト";
+
+const char D_08059a94[] = "こっそりラット";
+
+const char D_08059aa4[] = "リズム　おしゅうじ";
+
+const char D_08059ab8[] = "ドラムガールズ";
+
+const char D_08059ac8[] = "トスボーイズ　２";
+
+const char D_08059adc[] = "トスボーイズ";
+
+// Tram and Pauline
+const char D_08059aec[] = "トランとポリン";
+
+// Opening (A Type)
+const char D_08059afc[] = "オープニング　（Ａ　Ｔｙｐｅ）";
+
+// Opening (B Type)
+const char D_08059b1c[] = "オープニング　（Ｂ　Ｔｙｐｅ）";
+
+const char D_08059b3c[] = "ＲＩＱデータチェック";
+
+// Showtime
+const char D_08059b54[] = "ショータイム";
+
+const char D_08059b64[] = "コスモダンス";
+
+const char D_08059b74[] = "スペースダンス";
+
+const char D_08059b84[] = "ラップ　ウィメン（ｂｙ　ＫＡＺＵ）";
+
+const char D_08059ba8[] = "ラップ　ウィメン（ｂｙ　ＹＯＮＥ）";
+
+// Rap Men
+const char D_08059bcc[] = "ラップメン";
+
+// Quiz Show
+const char D_08059bd8[] = "クイズ";
+
+// Text
+const char D_08059be0[] = "テキスト";
+
+// Check Machine (Total)
+const char D_08059bec[] = "チェックマシン　（トータル）";
+
+const char D_08059c0c[] = "チェックマシン　（トリッキー）";
+
+const char D_08059c2c[] = "チェックマシン　（キューフ）";
+
+// Check Machine (Click)
+const char D_08059c4c[] = "チェックマシン　（クリック）";
+
+const char D_08059c6c[] = "スタジオドラマー";
+
+const char D_08059c80[] = "レッスン（じっせん８）";
+
+const char D_08059c98[] = "レッスン（じっせん７）";
+
+const char D_08059cb0[] = "レッスン（じっせん６）";
+
+const char D_08059cc8[] = "レッスン（じっせん５）";
+
+const char D_08059ce0[] = "レッスン（じっせん４）";
+
+const char D_08059cf8[] = "レッスン（じっせん３）";
+
+const char D_08059d10[] = "レッスン（じっせん２）";
+
+const char D_08059d28[] = "レッスン（じっせん１）";
+
+const char D_08059d40[] = "レッスン（スネアテクニック）";
+
+const char D_08059d60[] = "レッスン（はやい８ビート）";
+
+const char D_08059d7c[] = "レッスン（バスとスネア）";
+
+const char D_08059d98[] = "レッスン（スネア）";
+
+const char D_08059dac[] = "レッスン（ボッサ）";
+
+const char D_08059dc0[] = "レッスン（デスコビート）";
+
+const char D_08059ddc[] = "レッスン（キメパターン２）";
+
+const char D_08059df8[] = "レッスン（キメパターン１）";
+
+const char D_08059e14[] = "レッスン（ハード２）";
+
+const char D_08059e2c[] = "レッスン（ハード１）";
+
+const char D_08059e44[] = "レッスン（イージー）";
+
+const char D_08059e5c[] = "こえマシン（ラップ）";
+
+const char D_08059e74[] = "こえマシン（ラブ）";
+
+const char D_08059e88[] = "こえマシン（ネコ）";
+
+const char D_08059e9c[] = "ボンダンス";
+
+const char D_08059ea8[] = "ぼんおどり";
+
+const char D_08059eb4[] = "リミックス　１";
+
+const char D_08059ec4[] = "リミックス　２";
+
+const char D_08059ed4[] = "リミックス　３";
+
+const char D_08059ee4[] = "リミックス　４";
+
+const char D_08059ef4[] = "リミックス　５";
+
+const char D_08059f04[] = "リミックス　６";
+
+const char D_08059f14[] = "リミックス　７";
+
+const char D_08059f24[] = "リミックス　８";
+
+const char D_08059f34[] = "ウラおとこ";
+
+const char D_08059f40[] = "マネキンこうじょう";
+
+const char D_08059f54[] = "クイズ（スペシャル）";
+
+const char D_08059f6c[] = "バイキン　はかせ　ＳＰ";
+
+const char D_08059f84[] = "ライブ";
+
+const char D_08059f8c[] = "／";
