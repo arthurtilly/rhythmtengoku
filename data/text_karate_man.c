aa975a4b754fe316c1202dbc87f7577fc536fa06
GenericArrangements <genericarrangements.wah@gmail.com>
2022-11-15 11:07:52 +1100

    Decompile yet more of data_0805a5d0.s. Split game text into respective files.

---
+++ data/text_karate_man.c
@@ -0,0 +1,51 @@
+#include "global.h"
+
+extern const char D_0805ab50[];
+extern const char D_0805ab68[];
+extern const char D_0805ab6c[];
+extern const char D_0805ab90[];
+extern const char D_0805abb0[];
+extern const char D_0805abc4[];
+extern const char D_0805abdc[];
+extern const char D_0805abec[];
+extern const char D_0805ac0c[];
+extern const char D_0805ac60[];
+extern const char D_0805aca0[];
+extern const char D_0805acd8[];
+extern const char D_0805ad04[];
+extern const char D_0805ad38[];
+extern const char D_0805ad80[];
+
+
+/* Game Text - Karate Man */
+
+
+const char D_0805ab50[] = "きほんが　できてない。";
+
+const char D_0805ab68[] = "";
+
+const char D_0805ab6c[] = "つづけざまは　ニガテみたいだね。";
+
+const char D_0805ab90[] = "れんぞくパンチに　キレがある！";
+
+const char D_0805abb0[] = "さいご キマってた！";
+
+const char D_0805abc4[] = "\nノるのを　忘れずにネ！";
+
+const char D_0805abdc[] = "師匠のおことば";
+
+const char D_0805abec[] = "ようこそ！\nカラテ家のゲームへ。";
+
+const char D_0805ac0c[] = "このゲームは、Aボタンで「モノ」をパンチするだけだけどもタイミングが超シビアです。";
+
+const char D_0805ac60[] = "まずは、とんでくる\n「モノ」を　ビシッ　と\nパンチしてみよう。";
+
+const char D_0805aca0[] = "どう？\nタイミングのシビアさ、\nわかってもらえたかな？";
+
+const char D_0805acd8[] = "つぎは、音楽にあわせて\nパンチしてみよう！";
+
+const char D_0805ad04[] = "どうですか？\n音楽にノると、やりやすくないですか？";
+
+const char D_0805ad38[] = "では、そんなカンジで\n音楽にノッて「モノ」を\nビシビシ　たたきましょう！";
+
+const char D_0805ad80[] = "\n本番、スタート！！";
