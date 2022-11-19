d13e589b59cd6a779b80d89130378ea3bbe20e6b
GenericArrangements <genericarrangements.wah@gmail.com>
2022-11-19 16:58:01 +1100

    Decompile D_089380ac.

---
+++ data/data_089380ac.c
@@ -0,0 +1,22 @@
+#include "global.h"
+#include "data_089380ac.h"
+
+/* Related to Text Rendering */
+
+const struct struct_089380ac D_089380ac[] = {
+    {
+        D_08938264,
+        D_089631c4,
+        0x0a0b0118
+    },
+    {
+        D_08964e68,
+        D_0898fdc8,
+        0x0c0b0218
+    },
+    {
+        D_08991a6c,
+        D_089caeec,
+        0x100b0220
+    },
+};
---
+++ data/data_089380ac.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/* Related to Text Rendering */
+
+struct struct_089380ac {
+    const void *unk0;
+    const u8 *unk4;
+    u32 unk8;
+};
+
+extern const struct struct_089380ac D_089380ac[];
+
+extern const char D_089380d0[];
+extern const char D_089380d4[];
+extern const char D_089380e4[];
+extern const char D_089380e8[];
+extern const char D_08938138[];
+
+extern const u8 D_08938264[];
+extern const u8 D_089631c4[];
+
+extern const u8 D_08964e68[];
+extern const u8 D_0898fdc8[];
+
+extern const u8 D_08991a6c[];
+extern const u8 D_089caeec[];
