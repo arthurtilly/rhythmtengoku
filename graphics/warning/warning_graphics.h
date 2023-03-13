bdac670131f3e86c48d879e68e480cb581c4a30a
genericarrangements <genericarrangements.wah@gmail.com>
2023-03-13 15:49:06 +1100

    Decompile non-script data for RIQ Main Scene and Warning Screen.

--- graphics/warning/warning_graphics.h
+++ graphics/warning/warning_graphics.h
@@ -7,6 +7,11 @@
 
 extern struct Animation anim_warning_text_continue[];
 
+/* WARNING - COMPRESSED GRAPHICS */
+
+extern struct CompressedGraphics D_08d1ad4c;
+extern struct CompressedGraphics D_08d1b00c;
+
 /* WARNING - PALETTES */
 
 extern Palette warning_pal[];
