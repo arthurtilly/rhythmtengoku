39521bda554c9d473ec5b286f32a12df1bd07c19
EstexNT <estex64@gmail.com>
2023-10-02 04:19:44 +0300

    Finish decompiling code_08002f68

--- src/code_08002f68.h
+++ src/code_08002f68.h
@@ -7,17 +7,49 @@ struct struct_08003070 {
     u16 unk0_b1:1;
     u16 unk0_b2:14;
     u32 unk4;
-    u32 unk8;
-    u32 unkC;
+    u8 *unk8;
+    u16 *unkC;
     u16 unk10;
     u16 unk12;
+    u8 unk14;
+    u8 unk15;
+    u16 unk16;
+    u8 *unk18;
+    u8 *unk1C;
+    u8 unk20;
+    u8 unk21;
+    u8 unk22[8];
+    u8 unk2A[8];
+    u8 unk32[8];
+    u8 unk3A[8];
+    u16 *unk44;
+    u16 unk48;
+    u16 unk4A;
+    u8 unk4C;
+    u8 unk4D;
+    u16 unk4E;
+    u16 unk50;
+    u16 unk52;
+    u16 unk54;
+    u16 unk56;
+    u16 *unk58;
+    u16 *unk5C;
 };
 
-struct struct_08003974 {
-    u32 unk0;
+struct struct_080035a0 {
+    u8 unk0;
+    u32 unk4;
+    u32 unk8;
+    u32 unkC;
+    u8 *unk10;
+};
+
+struct struct_0800387c {
+    u8 *unk0;
     u8 unk4;
     u8 unk5;
 };
+
 
 extern void init_math_sqrt(void); // Init. sqrt()
 extern u8 func_08002f9c(s16 arg0, s16 arg1);
@@ -28,7 +60,7 @@ extern u16 func_08003004(s16 arg0, s16 arg1);
 // extern ? func_08003384(?);
 // extern ? func_08003398(?);
 // extern ? func_080033a0(?);
-// extern ? func_0800351c(?);
+extern void func_0800351c(struct struct_08003070 *, u8, u8, u8, u8);
 // extern ? func_08003598(?);
 // extern ? func_080035a0(?);
 // extern ? func_080036c4(?);
