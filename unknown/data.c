babdfbc2aa3b621c36fc6d0b5e70d0455abcd7a4
Shaffy <thedave2cz@gmail.com>
2023-10-22 15:51:02 +0200

    yay now it's shiftable and works kind of i think idk

---
+++ unknown/data.c
@@ -0,0 +1,164 @@
+#include "global.h"
+#include "graphics.h"
+
+#include "..\..\graphics\unknown\unknown_graphics.c"
+
+// [D_08a5dd8c] Graphics Table
+struct GraphicsTable unknown_gfx_table[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3A8B0,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3F16C,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD170,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5ddb0] Graphics Table 2
+struct GraphicsTable unknown_gfx_table2[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3B51C,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3F364,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD190,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5ddec] Graphics Table 3
+struct GraphicsTable unknown_gfx_table3[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3BE64,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3F580,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD1B0,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de1c] Graphics Table 3
+struct GraphicsTable unknown_gfx_table3[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3CA50,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3F798,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD1D0,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de4c] Graphics Table 4
+struct GraphicsTable unknown_gfx_table4[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3D6F4,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3F9CC,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD1F0,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de7c] Graphics Table 5
+struct GraphicsTable unknown_gfx_table5[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3E508,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3FC00,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD210,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5deac] Graphics Table 6
+struct GraphicsTable unknown_gfx_table6[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3EB4C,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3FDE4,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD230,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5dedc] Graphics Table 7
+struct GraphicsTable unknown_gfx_table7[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &D_08C3EF78,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &D_08C3FE9C,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* OBJ Tileset */ {
+        /* Src.  */ &D_088BD250,
+        /* Dest. */ 0x03004B64, //huh?
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
