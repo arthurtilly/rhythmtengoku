0eded42265099957c44f5895415ea3d4d342a460
Nibbl-z <thisisnotnibblz123@gmail.com>
2025-04-12 20:16:56 -0600

    name all functions and struct fields and all that

--- src/engines/rhythm_toys.c
+++ src/engines/rhythm_toys.c
@@ -7,20 +7,22 @@
 /* RHYTHM TOYS */
 
 
-void func_08044014(u32 arg0) {
+// Cat Machine Play Func.
+void rhythm_toys_cat_machine_play(u32 button) {
     struct DrumTechNote *sequence;
     
-    if (gRhythmToys->unk_358[arg0]) {
-        sequence = cat_machine_2_drum_seq_table[arg0];
+    if (gRhythmToys->buttonTimers[button] != 0) {
+        sequence = cat_machine_2_drum_seq_table[button];
     } else {
-        sequence = cat_machine_1_drum_seq_table[arg0];
+        sequence = cat_machine_1_drum_seq_table[button];
     }
 
     play_drumtech_seq(sequence, 0, 0);
-    gRhythmToys->unk_37c = play_sound_in_player(0, cat_machine_meow_sfx_table[arg0]);
+    gRhythmToys->soundPlayer = play_sound_in_player(0, cat_machine_meow_sfx_table[button]);
 }
 
-void func_08044074(u32 button) {
+// Rap Machine Play Func.
+void rhythm_toys_rap_machine_play(u32 button) {
     stop_sound(&s_rap_15ji_desuka_seqData);
     stop_sound(&s_rap_oyatu_desuka_seqData);
     stop_sound(&s_rap_wakari_desuka_seqData);
@@ -33,121 +35,136 @@ void func_08044074(u32 button) {
     stop_sound(&s_rap_orette_saiko_seqData);
     stop_sound(&s_rap_kimitte_saiko_seqData);
 
-    if (gRhythmToys->unk_382 == 0) {
-        gRhythmToys->unk_380 = agb_random(4);
+    if (gRhythmToys->changeRapMachineSfxTableTimer == 0) {
+        gRhythmToys->rapMachineSfxTable = agb_random(4);
     }
 
-    gRhythmToys->unk_382 = ticks_to_frames(0x24);
-    gRhythmToys->unk_37c = play_sound(rap_machine_sfx_pool[gRhythmToys->unk_380][button]);
+    gRhythmToys->changeRapMachineSfxTableTimer = ticks_to_frames(0x24);
+    gRhythmToys->soundPlayer = play_sound(rap_machine_sfx_pool[gRhythmToys->rapMachineSfxTable][button]);
 
     if (button == RHYTHM_TOYS_DOWN_BUTTON) {
-        if (gRhythmToys->unk_378) {
-            func_08044810();
+        if (gRhythmToys->musicPlaying) {
+            rhythm_toys_stop_music();
         } else {
-            if (gRhythmToys->unk_381) {
-                func_080447f4(&s_rap_machine_bgm_seqData);
+            if (gRhythmToys->playAltRapMachineSong) {
+                rhythm_toys_play_music(&s_rap_machine_bgm_seqData);
                 D_030053c0.musicTrkVolume = 0;
                 D_030053c0.musicTrkTargets = 0;
             } else {
-                func_080447f4(&s_rap_bgm_seqData);
+                rhythm_toys_play_music(&s_rap_bgm_seqData);
                 D_030053c0.musicTrkVolume = 0;
                 D_030053c0.musicTrkTargets = -23;
             }
 
-            gRhythmToys->unk_381 ^= 1;
+            gRhythmToys->playAltRapMachineSong ^= 1;
         }
     }
 }
 
-void func_080441cc(u32 button) {
-    if (gRhythmToys->unk_37c != NULL) {
-        fade_out_soundplayer(gRhythmToys->unk_37c, 0x1e);
+// Confession Machine Play Func.
+void rhythm_toys_love_machine_play(u32 button) {
+    if (gRhythmToys->soundPlayer != NULL) {
+        fade_out_soundplayer(gRhythmToys->soundPlayer, 0x1e);
     }
     
-    gRhythmToys->unk_37c = scene_play_random_sound(love_machine_sfx_table[button]);
+    gRhythmToys->soundPlayer = scene_play_random_sound(love_machine_sfx_table[button]);
 }
 
-// get button animation
-struct Animation* func_08044208(u32 arg0) {
-    return rhythm_toys_sprite_table[arg0][gRhythmToys->version].anim;
+// Get Sprite Animation
+struct Animation* rhythm_toys_get_sprite_anim(u32 sprite) {
+    return rhythm_toys_sprite_table[sprite][gRhythmToys->version].anim;
 }
 
-u16 func_08044228(u32 arg0) {
-    struct Vector2 *vec2 = rhythm_toys_sprite_table[arg0][gRhythmToys->version].vec2;
+// Get Sprite X
+u16 rhythm_toys_get_sprite_x(u32 sprite) {
+    struct Vector2 *vec2 = rhythm_toys_sprite_table[sprite][gRhythmToys->version].vec2;
     return vec2->x;
 }
 
-u16 func_08044248(u32 arg0) {
-    struct Vector2 *vec2 = rhythm_toys_sprite_table[arg0][gRhythmToys->version].vec2;
+// Get Sprite Y
+u16 rhythm_toys_get_sprite_y(u32 sprite) {
+    struct Vector2 *vec2 = rhythm_toys_sprite_table[sprite][gRhythmToys->version].vec2;
     return vec2->y;
 }
 
-void func_08044268(void) {
-    gRhythmToys->unk_35f = rhythm_toys_demo_table[gRhythmToys->version] == 0;
-    gRhythmToys->unk_360 = sprite_create(gSpriteHandler, func_08044208(6), 0, func_08044228(6), func_08044248(6), 0x800, 0, 0, 0);
-    gRhythmToys->unk_362 = 0;
+// Init. Demo
+void rhythm_toys_init_demo(void) {
+    gRhythmToys->hasNoDemo = rhythm_toys_demo_table[gRhythmToys->version] == NULL;
+    gRhythmToys->demoSprite = sprite_create(gSpriteHandler, 
+        rhythm_toys_get_sprite_anim(RHYTHM_TOYS_SPRITE_DEMO_ICON), 0, 
+        rhythm_toys_get_sprite_x(RHYTHM_TOYS_SPRITE_DEMO_ICON), 
+        rhythm_toys_get_sprite_y(RHYTHM_TOYS_SPRITE_DEMO_ICON), 
+        0x800, 0, 0, 0
+    );
 
-    if (gRhythmToys->unk_35f) {
-        sprite_set_visible(gSpriteHandler, gRhythmToys->unk_360, FALSE);
+    gRhythmToys->demoEnabled = FALSE;
+
+    if (gRhythmToys->hasNoDemo) {
+        sprite_set_visible(gSpriteHandler, gRhythmToys->demoSprite, FALSE);
     }
 }
 
-void func_08044320(u32 arg0) {
-    if (gRhythmToys->unk_35f == 0) {
-        if (gRhythmToys->unk_362) {
-            func_080443c0();
+// Toggle Demo
+void rhythm_toys_toggle_demo(u32 currentMachine) {
+    if (gRhythmToys->hasNoDemo == 0) {
+        if (gRhythmToys->demoEnabled) {
+            rhythm_toys_end_demo();
         }
 
-        sprite_set_playback(gSpriteHandler, gRhythmToys->unk_360, 1, 0, 0);
-        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->unk_360, 1);
+        sprite_set_playback(gSpriteHandler, gRhythmToys->demoSprite, 1, 0, 0);
+        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->demoSprite, 1);
 
-        gRhythmToys->unk_362 = 1;
+        gRhythmToys->demoEnabled = TRUE;
 
-        gRhythmToys->currentDemoAction = rhythm_toys_demo_table[arg0];
-        gRhythmToys->unk_368 = 0;
-        func_08044810();
+        gRhythmToys->currentDemoAction = rhythm_toys_demo_table[currentMachine];
+        gRhythmToys->demoDeltaTime = 0;
+        rhythm_toys_stop_music();
     }
 }
 
-void func_080443c0(void) {
-    if (gRhythmToys->unk_35f == 0 && gRhythmToys->unk_362 != 0) {
-        sprite_set_playback(gSpriteHandler, gRhythmToys->unk_360, 0, 0, 0);
-        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->unk_360, 0);
+// End Demo
+void rhythm_toys_end_demo(void) {
+    if (gRhythmToys->hasNoDemo == 0 && gRhythmToys->demoEnabled) {
+        sprite_set_playback(gSpriteHandler, gRhythmToys->demoSprite, 0, 0, 0);
+        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->demoSprite, 0);
         stop_drumtech();
-        gRhythmToys->unk_362 = 0;
-        func_08044810();
+        gRhythmToys->demoEnabled = FALSE;
+        rhythm_toys_stop_music();
     }
 }
 
-void func_0804442c(void) {
+// Update Demo
+void rhythm_toys_update_demo(void) {
     u16 button;
-    
-    if (gRhythmToys->unk_35f == 0 && gRhythmToys->unk_362 != 0) {
-        gRhythmToys->unk_368 -= func_0800c398();
 
-        while (gRhythmToys->unk_368 < 0) {
+    if (gRhythmToys->hasNoDemo == 0 && gRhythmToys->demoEnabled) {
+        gRhythmToys->demoDeltaTime -= func_0800c398();
+
+        while (gRhythmToys->demoDeltaTime < 0) {
             button = gRhythmToys->currentDemoAction->button;
             
-            if (button == 0xFFFF) {
-                func_080443c0();
+            if (button == (u16)RHYTHM_TOYS_DEMO_STOP) {
+                rhythm_toys_end_demo();
                 return;
             }
 
-            if (button < 6) {
-                func_0804475c(button);
+            if (button < RHYTHM_TOYS_DEMO_REST) {
+                rhythm_toys_button_press(button);
             }
 
-            gRhythmToys->unk_368 += (gRhythmToys->currentDemoAction->deltaTime) * 0x100;
+            gRhythmToys->demoDeltaTime += (gRhythmToys->currentDemoAction->deltaTime) * 0x100;
             gRhythmToys->currentDemoAction++;
         }
     }
 }
 
+// Graphics Init. 3
 void rhythm_toys_init_gfx3(void) {
     func_0800c604(0);
     gameplay_start_screen_fade_in();
 }
 
+// Graphics Init. 2
 void rhythm_toys_init_gfx2(void) {
     s32 task;
 
@@ -156,6 +173,7 @@ void rhythm_toys_init_gfx2(void) {
     run_func_after_task(task, rhythm_toys_init_gfx3, 0);
 }
 
+// Graphics Init. 1
 void rhythm_toys_init_gfx1(void) {
     s32 task;
 
@@ -164,6 +182,7 @@ void rhythm_toys_init_gfx1(void) {
     run_func_after_task(task, rhythm_toys_init_gfx2, 0);
 }
 
+// Game Engine Start
 void rhythm_toys_engine_start(u32 version) {
     u32 i;
     s16 sprite;
@@ -175,106 +194,112 @@ void rhythm_toys_engine_start(u32 version) {
     scene_show_obj_layer();
     scene_set_bg_layer_display(BG_LAYER_1, TRUE, 0, 0, 0, 29, 1);
     
-    gRhythmToys->unk_354 = sprite_create(gSpriteHandler, 
-        func_08044208(RHYTHM_TOYS_SPRITE_MACHINE_R), 0x7f,    
-        func_08044228(RHYTHM_TOYS_SPRITE_MACHINE_R),
-        func_08044248(RHYTHM_TOYS_SPRITE_MACHINE_R),
+    gRhythmToys->characterSpriteR = sprite_create(gSpriteHandler, 
+        rhythm_toys_get_sprite_anim(RHYTHM_TOYS_SPRITE_MACHINE_R), 0x7f,    
+        rhythm_toys_get_sprite_x(RHYTHM_TOYS_SPRITE_MACHINE_R),
+        rhythm_toys_get_sprite_y(RHYTHM_TOYS_SPRITE_MACHINE_R),
         0x800, 1, 0x7f, 0
     );
 
     if (version == ENGINE_VER_RAP_MACHINE) {
-        gRhythmToys->unk_356 = sprite_create(gSpriteHandler, 
-            func_08044208(RHYTHM_TOYS_SPRITE_MACHINE_L), 0x7f,
-            func_08044228(RHYTHM_TOYS_SPRITE_MACHINE_L),
-            func_08044248(RHYTHM_TOYS_SPRITE_MACHINE_L),
+        gRhythmToys->characterSpriteL = sprite_create(gSpriteHandler, 
+            rhythm_toys_get_sprite_anim(RHYTHM_TOYS_SPRITE_MACHINE_L), 0x7f,
+            rhythm_toys_get_sprite_x(RHYTHM_TOYS_SPRITE_MACHINE_L),
+            rhythm_toys_get_sprite_y(RHYTHM_TOYS_SPRITE_MACHINE_L),
             0x7ff, 1, 0x7f, 0
         );
     } else {
-        gRhythmToys->unk_356 = gRhythmToys->unk_354; // ?
+        gRhythmToys->characterSpriteL = gRhythmToys->characterSpriteR;
     }
 
     init_drumtech(&gRhythmToys->drumtech);
 
     for (i = 0; i < 6; i++) {
-        gRhythmToys->unk_358[i] = 0;
+        gRhythmToys->buttonTimers[i] = 0;
     }
 
     gameplay_set_input_buttons(0, 0);
 
     for (i = 0; i < 6; i++) {
-        gRhythmToys->unk_36c[i] = sprite_create(gSpriteHandler,
-            func_08044208(rhythm_toys_button_anim_ids[i]), 0x7f,
-            func_08044228(rhythm_toys_button_anim_ids[i]),
-            func_08044248(rhythm_toys_button_anim_ids[i]),
+        gRhythmToys->buttonSprites[i] = sprite_create(gSpriteHandler,
+            rhythm_toys_get_sprite_anim(rhythm_toys_button_anim_ids[i]), 0x7f,
+            rhythm_toys_get_sprite_x(rhythm_toys_button_anim_ids[i]),
+            rhythm_toys_get_sprite_y(rhythm_toys_button_anim_ids[i]),
             0x700, 1, 0x7f, 0
         );
     }
 
     gameplay_prevent_dpad_overlap(FALSE);
-    func_08044268();
+    rhythm_toys_init_demo();
 
-    gRhythmToys->unk_35e = 0;
-    gRhythmToys->unk_378 = 0;
-    gRhythmToys->unk_37c = NULL;
-    gRhythmToys->unk_382 = 0;
-    gRhythmToys->unk_381 = 0;
-    gRhythmToys->unk_380 = agb_random(4);
+    gRhythmToys->updateDisabled = FALSE;
+    gRhythmToys->musicPlaying = FALSE;
+    gRhythmToys->soundPlayer = NULL;
+    gRhythmToys->changeRapMachineSfxTableTimer = 0;
+    gRhythmToys->playAltRapMachineSong = FALSE;
+    gRhythmToys->rapMachineSfxTable = agb_random(4);
 }
 
+// Engine Event 0x00 (Stub)
 void rhythm_toys_engine_event_stub(void) {
 }
 
-void func_08044730(u32 arg0) {
-    gRhythmToys->unk_358[arg0] = clamp_int32(ticks_to_frames(6) - 2, 0, 10);
+// Reset Button Timer
+void rhythm_toys_reset_button_timer(u32 button) {
+    gRhythmToys->buttonTimers[button] = clamp_int32(ticks_to_frames(6) - 2, 0, 10);
 }
 
-void func_0804475c(s32 arg0) {
-    s16 unk;
+// Press Button
+void rhythm_toys_button_press(s32 button) {
+    s16 sprite;
     
-    if (-1 < arg0) {
-        if (arg0 < 2) {
-            unk = gRhythmToys->unk_354;
+    if (RHYTHM_TOYS_DEMO_STOP < button) {
+        if (button < RHYTHM_TOYS_UP_BUTTON) {
+            sprite = gRhythmToys->characterSpriteR;
         } else {
-            unk = gRhythmToys->unk_356;
+            sprite = gRhythmToys->characterSpriteL;
         }
 
-        sprite_set_anim(gSpriteHandler, unk, func_08044208(rhythm_toys_machine_anim_ids[arg0]), 0, 1, 0x7f, 0);
-        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->unk_36c[arg0], 0);   
-        rhythm_toys_play_func_table[gRhythmToys->version](arg0);
+        sprite_set_anim(gSpriteHandler, sprite, rhythm_toys_get_sprite_anim(rhythm_toys_machine_anim_ids[button]), 0, 1, 0x7f, 0);
+        sprite_set_anim_cel(gSpriteHandler, gRhythmToys->buttonSprites[button], 0);   
+        rhythm_toys_play_func_table[gRhythmToys->version](button);
     }
 }
 
-void func_080447f4(struct SongHeader *song) {
+// Play Music
+void rhythm_toys_play_music(struct SongHeader *song) {
     scene_set_music(song);
-    gRhythmToys->unk_378 = 1;
+    gRhythmToys->musicPlaying = TRUE;
 }
 
-void func_08044810(void) {
+// Stop Music
+void rhythm_toys_stop_music(void) {
     scene_stop_music();
-    gRhythmToys->unk_378 = 0;
+    gRhythmToys->musicPlaying = FALSE;
 }
 
+// Game Engine Update
 void rhythm_toys_engine_update(void) {
     u32 i;
     s32 button;
     
-    if (gRhythmToys->unk_35e == FALSE) {
-        set_drumtech_note_func(func_08044730);
+    if (gRhythmToys->updateDisabled == FALSE) {
+        set_drumtech_note_func(rhythm_toys_reset_button_timer);
 
         for (i = 0; i < 6; i++) {
-            if (gRhythmToys->unk_358[i]) {
-                gRhythmToys->unk_358[i]--;
+            if (gRhythmToys->buttonTimers[i] != 0) {
+                gRhythmToys->buttonTimers[i]--;
             }
         }
 
-        if (gRhythmToys->unk_362) {
-            func_0804442c();
+        if (gRhythmToys->demoEnabled) {
+            rhythm_toys_update_demo();
             
             if (D_03004afc & START_BUTTON) {
-                func_080443c0();
+                rhythm_toys_end_demo();
             }
         } else {
-            button = -1;
+            button = RHYTHM_TOYS_DEMO_STOP;
 
             if (D_03004afc & DPAD_RIGHT) {
                 button = RHYTHM_TOYS_RIGHT_BUTTON;
@@ -300,10 +325,10 @@ void rhythm_toys_engine_update(void) {
                 button = RHYTHM_TOYS_A_BUTTON;
             }
 
-            func_0804475c(button);
+            rhythm_toys_button_press(button);
 
             if (D_03004afc & START_BUTTON) {
-                func_08044320(gRhythmToys->version);
+                rhythm_toys_toggle_demo(gRhythmToys->version);
             }
         }
 
@@ -313,27 +338,30 @@ void rhythm_toys_engine_update(void) {
             stop_drumtech();
             beatscript_disable_loops();
 
-            gRhythmToys->unk_35e = 1;
+            gRhythmToys->updateDisabled = TRUE;
 
-            if (gRhythmToys->unk_37c != NULL) {
-                fade_out_soundplayer(gRhythmToys->unk_37c, 10);
+            if (gRhythmToys->soundPlayer != NULL) {
+                fade_out_soundplayer(gRhythmToys->soundPlayer, 10);
             }
 
             play_sound(&s_menu_cancel2_seqData);
         }
 
-        if (gRhythmToys->unk_382) {
-            gRhythmToys->unk_382--;
+        if (gRhythmToys->changeRapMachineSfxTableTimer) {
+            gRhythmToys->changeRapMachineSfxTableTimer--;
         }
     }
 }
 
+// Game Engine Stop
 void rhythm_toys_engine_stop(void) {
 }
 
+// Cue - Spawn
 void rhythm_toys_cue_spawn(struct Cue *cue, struct RhythmToysCue *info, u32 param) {
 }
 
+// Cue - Update
 u32 rhythm_toys_cue_update(struct Cue *cue, struct RhythmToysCue *info, u32 runningTime, u32 released) {
     if (runningTime > ticks_to_frames(0x78)) {
         return TRUE;
@@ -342,26 +370,34 @@ u32 rhythm_toys_cue_update(struct Cue *cue, struct RhythmToysCue *info, u32 runn
     }
 }
 
+// Cue - Despawn
 void rhythm_toys_cue_despawn(struct Cue *cue, struct RhythmToysCue *info) {
 }
 
+// Cue - Hit
 void rhythm_toys_cue_hit(struct Cue *cue, struct RhythmToysCue *info, u32 pressed, u32 released) {
 }
 
+// Cue - Barely
 void rhythm_toys_cue_barely(struct Cue *cue, struct RhythmToysCue *info, u32 pressed, u32 released) {
 }
 
+// Cue - Miss
 void rhythm_toys_cue_miss(struct Cue *cue, struct RhythmToysCue *info) {
 }
 
+// Input Event
 void rhythm_toys_input_event(u32 pressed, u32 released) {
 }
 
+// Common Event 0 (Beat Animation, Unimplemented)
 void rhythm_toys_common_beat_animation(void) {
 }
 
+// Common Event 1 (Display Text, Unimplemented)
 void rhythm_toys_common_display_text(void) {
 }
 
+// Common Event 2 (Init. Tutorial, Unimplemented)
 void rhythm_toys_common_init_tutorial(void) {
 }
